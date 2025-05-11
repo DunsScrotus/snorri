e1d397b03ea05d84d213fd7e45a0529f83cccb66
liminalfun <xelanawotka@gmail.com>
2025-05-10 20:55:03 -0400

    Deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+// Deploying from Github Pages
+// https://docs.astro.build/en/guides/deploy/github/
+export default defineConfig({
+	site: 'https://snorri-space.github.io',
+	base: 'snorri'
+});
